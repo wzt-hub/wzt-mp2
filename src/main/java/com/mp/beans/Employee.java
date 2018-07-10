70cb21632bec0a0af6221794377c057c8eb3d58d
wzt <2444722463@qq.com>
2018-07-10 19:49:34 +0800

    MybatisPlus ActiveRecord

--- src/main/java/com/mp/beans/Employee.java
+++ src/main/java/com/mp/beans/Employee.java
@@ -3,33 +3,10 @@ package com.mp.beans;
 import java.util.Date;
 
 import com.baomidou.mybatisplus.annotations.TableField;
-import com.baomidou.mybatisplus.annotations.TableId;
-import com.baomidou.mybatisplus.annotations.TableName;
-import com.baomidou.mybatisplus.enums.IdType;
 
-/**
- *  定义JavaBean中成员变量时所使用的类型
- *  因为每一个基本数据类型都有一个默认值
- *  int  --> 0
- *  boolean --> false
- *   包装类型使用更符合实际：null
- *   学生考试，如果没有参加则应该是null，和参加考试只考了0分，意义则不同 
- * @author Administrator
- */
-
-/**
- * Mybatis-Plus会默认使用实体类的类名到数据库中找对应的表
- */
-//如果在数据库中所有的表具有相同的前缀，则可以在applicationContext.xml文件中配置全局的表前缀策略
-//@TableName(value="tbl_employee")    
+ 
 public class Employee {
-	/**
-	 * @TableId
-	 * 	value：指定表中的主键列的列名，如果实体属性名与列名一致，可以省略不指定
-	 *  type：指定主键策略
-	 */
-	//可以在applicationContext.xml文件中配置全局的主键策略，这样就不必每一个实体类单独配置
-	//@TableId(value="id",type=IdType.AUTO)
+	
 	private Integer id ;
 	private String lastName;
 	private String email ;
--- src/main/java/com/mp/mapper/EmployeeMapper.java
+++ src/main/java/com/mp/mapper/EmployeeMapper.java
@@ -3,26 +3,6 @@ package com.mp.mapper;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.mp.beans.Employee;
 
-/**
- * Mapper接口：
- * 1.基于MyBatis：在Mapper接口中编写CRUD相关的方法，还需要提供Mapper接口
- * 		所对应的SQL映射文件，以及方法对应的SQL语句。
- * 2.基于MyBatis-Plus：只需要让Mapper接口继承BaseMapper接口即可
- * 		BaseMapper<T> ：泛型T指的是当前Mapper接口所操作的实体类的类型
- * 3.Mapper 继承该接口后，无需编写 mapper.xml 文件，即可获得CRUD功能
- * @author Administrator
- *
- */
 public interface EmployeeMapper extends BaseMapper<Employee> {
-	/**
-	 * 原生Mybatis插入一行数据之后，获取其主键值的方法：
-	 * 1.首先在EmployeeMapper接口中定义方法： Integer insertEmployee(Employee employee);
-	 * 2.在EmployeeMapper接口对应的xml文件中：使用useGeneratedKeys=true keyProperty="id" 来获取自增主键
-	 * 		<insert useGeneratedKeys=true keyProperty="id">
-	 * 			SQL语句...
-	 * 		</insert>
-	 * 如果为Mybatis-Plus 的话，则不需要做任何操作，因为Mybatis-Plus会自动的将自增主键值回写到实体类中
-	 */
-	
 	
 }
